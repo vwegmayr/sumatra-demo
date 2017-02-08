306546b2e75cdae006696b456b9013f91f3f32aa
vikweg <vwegmayr@inf.ethz.ch>
2017-02-08 17:30:57 +0100

    Configure test suite

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import numpy as np 
+from scipy import misc
+import ConfigParser
 
-data = np.random.uniform(size=100)
+config = ConfigParser.ConfigParser()
 
-np.save("Data/rnddata.npy",data)
+config.read(".config")
+
+data = np.random.uniform(size=config.getint("sec_one","num"))
+img = misc.imread("img.jpg")
+
+misc.imsave("Data/img1.png",img)
+misc.imsave("Data/img2.png",img)
+misc.imsave("Data/img3.png",img)
+
+np.save("Data/rnddata1.npy",data)
+np.save("Data/rnddata2.npy",data)
