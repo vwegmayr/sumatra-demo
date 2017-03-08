d5a4d7518ab7bccbe9674bb0d9b8d9f995b0f18d
vwegmayr <vwegmayr@inf.ethz.ch>
2017-03-08 14:02:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import numpy as np 
 import ConfigParser
 
-config = ConfigParser.ConfigParser()
+PATH = "full/path/to/Dropbox/ise-squad/"
 
+# Read the config file
+config = ConfigParser.ConfigParser()
 config.read(".config")
 
+# Produce some data
 data = np.random.uniform(size=config.getint("sec_one","num"))
 img_in_file = open("img.jpg","rb")
 img = img_in_file.read()
 
-path = "full/path/to/Dropbox/ise-squad/"
-
-img_out_file = open(path+"img1.png","wb")
+# Produe some output
+img_out_file = open(PATH+"img1.png","wb")
 img_out_file.write(img)
-np.save(path+"rnddata1.npy",data)
-np.save(path+"rnddata2.npy",data)
+np.save(PATH+"rnddata1.npy",data)
+np.save(PATH+"rnddata2.npy",data)
